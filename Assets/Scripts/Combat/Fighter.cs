472813dac16cc58ba44a50c2fbc8756d4af1fe95
YacineL <yacine9258@gmail.com>
2019-09-15 07:54:11 +0100

    [FIX] : adds the ennemy model in a prefab variant of character

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -9,11 +9,16 @@ namespace TWQ.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 10f;
 
         Transform target;
+        float timeSinceLastAttack = 0;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
 
 
@@ -24,8 +29,24 @@ namespace TWQ.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
-                Cancel();
+                AttackBehaviour();
             }
+        }
+
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+                
+            }
+        }
+        //Called by the animator
+        void Hit()
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(10f);
         }
 
         public void Attack(CombatTarget combatTarget)
---
+++ Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace TWQ.Combat
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField] float health = 100f;
+        
+        public void TakeDamage(float damage)
+        {
+            health = Mathf.Max(health - damage, 0);
+        }
+    }
+}
+
