821842144112e2d0dc3631626fbb90b0e87792f7
YacineL <yacine9258@gmail.com>
2019-09-23 22:35:11 +0100

    [ENH] : makes guards return to their position after losing the player
    with a suspicion time

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using TWQ.Combat;
 using TWQ.Core;
+using TWQ.Movement;
 using UnityEngine;
 
 namespace TWQ.Control
@@ -9,14 +10,23 @@ namespace TWQ.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 3f;
+
         Fighter fighter;
         Health health;
         GameObject player;
+        Mover mover;
+
+        Vector3 guardPosition;
+        float timeSinceLastSaw = Mathf.Infinity;
         private void Start()
         {
             player = GameObject.FindWithTag("Player");
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
+
+            guardPosition = transform.position;
         }
         private void Update()
         {
@@ -24,19 +34,46 @@ namespace TWQ.Control
 
             if ( InAttackRangeOfPlayer() && fighter.CanAttack(player))
             {
-                fighter.Attack(player);
+                timeSinceLastSaw = 0;
+                AttackBehaviour();
+            }
+            else if(timeSinceLastSaw < suspicionTime)
+            {
+                SuspicionBehaviour();
             }
             else
             {
-                fighter.Cancel();
+                GuardBehaviour();
             }
-            
+            timeSinceLastSaw += Time.deltaTime;
+        }
+
+        private void GuardBehaviour()
+        {
+            fighter.Cancel();
+            mover.StartMoveAction(guardPosition);
+        }
+
+        private void SuspicionBehaviour()
+        {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void AttackBehaviour()
+        {
+            fighter.Attack(player);
         }
 
         private bool InAttackRangeOfPlayer()
         {
             float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
             return distanceToPlayer < chaseDistance;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
         }
     }
 }
