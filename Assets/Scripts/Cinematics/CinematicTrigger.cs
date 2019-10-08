a788496f0ba80f33887703bc0e6efcfee867447f
YacineL <yacine9258@gmail.com>
2019-10-08 02:21:46 +0100

    [ENH] : shows a message in the console when the cinematic begin and ends
    messages will be replaced by enabling and disabling player controls

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+using UnityEngine.Playables;
+namespace TWQ.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool alreadyTriggered = false;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!alreadyTriggered && other.gameObject.tag == "Player")
+            {
+                alreadyTriggered = true; 
+                GetComponent<PlayableDirector>().Play();
+            }
+        }
+    }
+}
