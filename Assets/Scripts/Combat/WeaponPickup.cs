6dd8a38f1fae46fafbbb80b6179df2899b9e274d
YacineL <yacine9258@gmail.com>
2019-10-31 12:54:12 +0100

    [FIX] : fixes samll bugs in the navmesh agent
    -and making the player move with directional keys( joystick ) again.

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +8,29 @@ namespace TWQ.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon = null;
+        [SerializeField] float respawnTime = 4f;
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player")
             {
                 other.GetComponent<Fighter>().EquppingWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
+            }
+        }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+
+        private void ShowPickup(bool shouldShow)
+        {
+            GetComponent<Collider>().enabled = false;
+            foreach(Transform child in transform)
+            {
+                child.gameObject.SetActive(shouldShow);
             }
         }
     }
